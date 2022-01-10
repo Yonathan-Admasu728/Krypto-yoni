65e220e25d70a747f35169af643fe6d74391705b
yonathan Admasu <admyoni4@yahoo.com>
2022-01-10 06:23:27 -0800

    initialized the project using vite and successfully installed and integrated tailwindcss

---
+++ client/postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+// const tailwindcss = require("tailwindcss");
+// module.exports = {
+//   plugins: [tailwindcss("./tailwind.config.js"), require("autoprefixer")],
+// };
